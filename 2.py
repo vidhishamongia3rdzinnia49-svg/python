6784cb050c00a47f8a92bd53fc25ca1341f8bb81
vidhishamongia3rdzinnia49-svg <vidhishamongia3rdzinnia49@gmail.com>
2026-03-10 13:59:28 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,4 @@
+print("HELLO WORLD !")
+print("MY NAME IS VIDHISHA !")
+print("I AM IN CLASS 8")
+print("THE RESULT OF 7 x 8 IS ",7 * 8)
---
+++ 4.py
@@ -0,0 +1,11 @@
+x=5
+y="JOHN"
+print(x)
+print(y)
+codingal="CODINGAL"
+print(codingal)
+name=input("enter your name")
+print("\nhello",name,"\nwelcome to codingal")
+print("hello",name,"howare you")
+print("hello{},howare you".format(name))
+print(f"hello{name},howare you")
